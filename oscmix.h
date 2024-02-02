c830a6cc5247c50b9bb625f463d454f97cfad8c9
Michael Forney <mforney@mforney.org>
2024-02-02 15:24:34 -0800

    Add forgotten oscmix.h

---
+++ oscmix.h
@@ -0,0 +1,13 @@
+#ifndef OSCMIX_H
+#define OSCMIX_H
+
+void init(void);
+
+void handlesysex(unsigned char *buf, size_t len, uint32_t *payload);
+int handleosc(unsigned char *buf, size_t len);
+void handletimer(bool levels);
+
+extern int writemidi(const void *buf, size_t len);
+extern void writeosc(const void *buf, size_t len);
+
+#endif
