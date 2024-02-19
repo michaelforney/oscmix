52ded4aa36492f74049bffd75e1d49a645de3b66
Michael Forney <mforney@mforney.org>
2024-02-18 20:22:44 -0800

    gtk: Partially fix build with older glib
    Remaining issues include use of G_DEFINE_ENUM_TYPE and G_DEFINE_FLAGS_TYPE.

--- gtk/main.c
+++ gtk/main.c
@@ -664,7 +664,13 @@ main(int argc, char *argv[])
 	GtkApplication *app;
 	int ret;
 
-	app = gtk_application_new(NULL, G_APPLICATION_DEFAULT_FLAGS);
+	app = gtk_application_new(NULL,
+#if GLIB_CHECK_VERSION(2, 74, 0)
+		G_APPLICATION_DEFAULT_FLAGS
+#else
+		G_APPLICATION_FLAGS_NONE
+#endif
+	);
 	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 	ret = g_application_run(G_APPLICATION(app), argc, argv);
 	g_object_unref(app);
